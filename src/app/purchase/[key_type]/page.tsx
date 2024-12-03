09ee0ab39f32baec494368fbea22ef71d419f7b8
MsDoors.gg <msdoors.gg@gmail.com>
2024-12-03 15:44:16 -0300

    Update page.tsx

--- src/app/purchase/[key_type]/page.tsx
+++ src/app/purchase/[key_type]/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation"
 
 const products = {
-    "lifetime": "https://mspaint.sell.app/product/mspaint-lifetime-key?store=mspaint&quantity=1&data=faq"
+    "lifetime-free: "https://dsc.gg/msdoors-gg"
 }
 
 export default function Page({ params }: { params: { key_type: string } }) {
